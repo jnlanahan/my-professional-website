7c4debbc9f106fc228e999254d29e4fc1976b0ed
jnlanahan <jnlanahan@gmail.com>
2024-05-22 21:57:59 -0400

    Update scripts.js
    Dont know what I am doing

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function highlightCurrentSection() {
         let index = sections.length;
 
-        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
 
         navLinks.forEach((link) => link.classList.remove('active'));
         navLinks[index].classList.add('active');
